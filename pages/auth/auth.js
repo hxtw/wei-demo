296f0d6b30d9824c756e9e4238e6715f8325e762
huangxin <827015448@qq.com>
2018-08-07 18:05:18 +0800

    逻辑层

---
+++ pages/auth/auth.js
@@ -0,0 +1,15 @@
+/**
+ * @Author : Huangx
+ * @Date : 2018/8/7
+ **/
+const app = getApp()
+
+Page({
+    data:{},
+    //事件处理函数
+    bindViewTap: function () {
+        wx.navigateTo({
+            // url: '../logs/logs'
+        })
+    },
+});
